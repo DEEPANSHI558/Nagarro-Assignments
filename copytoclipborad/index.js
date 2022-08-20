1154a64196aeb6ba62a74ed82d7c5722bcf42698
HornyWaifu69 <abc@e.com>
2022-08-21 02:10:21 +0530

    Revert "add twitter clone"
    This reverts commit fb4f43e9ee7cec99e6e1b5ec875501395b690786.
    
    add twitter clone
    
    add twitter clone
    
    add twitter clone
    
    Revert "add twitter clone"
    
    This reverts commit 89eb2def4ea2a95560ddfe75a704a07348bab6d0.
    
    Revert "Revert "add twitter clone""
    
    This reverts commit 64678bb8d3b04a08b61ca895e20f249e448e73dc.

--- copytoclipborad/index.js
+++
@@ -1,6 +0,0 @@
-function myFunction(){
-    let text=document.getElementById('text');
-    text.selected();
-    text.setSelectionRange(0,1000); //it takes two parameters count of the starting alphabet and the ending
-
-}
--- rest_api/routes/movie.js
+++
@@ -1,9 +0,0 @@
-const express=require("express");
-const { getMovie, home, getAll, postMovie } = require("../controllers/movie");
-const router = express.Router();
-
-router.get("/",home);
-router.get("/all",getAll);
-router.get("/movie/:id",getMovie)
-router.post("/",postMovie);
-module.exports = router;
